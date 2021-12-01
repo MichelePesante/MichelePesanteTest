faf8b3533fceecede8de37ddb7195b31c2ad52c3
Michele Pesante <m.pesante@avanade.com>
2021-12-02 00:40:25 +0100

    Created first frontend version

--- MichelePesanteTest/Models/DocumentModel.cs
+++ MichelePesanteTest/Models/DocumentModel.cs
@@ -20,6 +20,5 @@ namespace MichelePesanteTest.Models
         public string Type { get; set; }
         public string ExecutionTime { get; set; }
         public string Result { get; set; }
-
     }
 }
--- MichelePesanteTest/Pages/Tracker.cshtml.cs
+++ MichelePesanteTest/Pages/Tracker.cshtml.cs
@@ -1,19 +1,16 @@
-﻿using MichelePesanteTest.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MichelePesanteTest.Pages
 {
-    public class IndexModel : PageModel
+    public class Tracker : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }
