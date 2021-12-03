5d14de1f59efdc7133b0e090e8499eef4c7c9104
Michele Pesante <m.pesante@avanade.com>
2021-12-03 13:03:18 +0100

    Updated DocumentResponseModel to show CategoriesInfo before Documents

--- MichelePesanteTest/Models/DocumentResponseModel.cs
+++ MichelePesanteTest/Models/DocumentResponseModel.cs
@@ -5,7 +5,7 @@ namespace MichelePesanteTest.Models
     public class DocumentResponseModel
     {
         public int DocumentsNumber { get; set; }
-        public List<DocumentModel> Documents { get; set; }
         public Dictionary<string, int> CategoriesInfo { get; set; }
+        public List<DocumentModel> Documents { get; set; }
     }
 }
